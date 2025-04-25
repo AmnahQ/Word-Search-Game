bff2b1e9bc36d82172460011619d04748cc36db9
AmnahQ <122616031+AmnahQ@users.noreply.github.com>
2025-04-25 17:42:48 +0500

    Update my_trie.py

--- final/my_trie.py
+++ final/my_trie.py
@@ -1,58 +1,45 @@
 class TrieNode:
-	
-	# Each node in the trie has a value, a list of children, 
-	# and a "complete" value that keeps track of whether that
-	# node completes a word in the trie. It contains "None" if 
-	# it doesn't. If it does, it contains the word, which will
-	# be equal to the concatenation of the values of all the
-	# nodes on the path from the root to this node
+	#attributes of trie
 	def __init__(self, value):
 		self.value = value
 		self.children = []
 		self.complete = None
 
-	# Adds a child to the node
+	#node child added here
 	def add(self, child):
 		self.children.append(child)
 
-	# Returns the child node with the requested value if it
-	# exists, and returns None otherwise. Allows us to search
-	# the trie for a word letter-by-letter
+	#returns the value of the child if it exists
 	def get_child(self, value):
 		for child in self.children:
 			if child.value == value:
 				return child
 		return None
 
-	# Tells Python that, when we call "print" on a node,
-	# we want to print its value field
+	#prints value
 	def __str__(self):
 		return str(self.value)
 
 
 class Trie:
 	
-	# The root of a trie is the empty string
-	def __init__(self, listy=None):
+	#root is empty string 
+	def __init__(self, list_words=None):
 		self.root = TrieNode('')
 
-		# If we pass in the optional listy parameter, 
-		# the list of words is added to the trie
-		if listy != None:
-			self.add_to_trie(listy)
+		# Adds the words in the list to the trie
+		if list_words != None:
+			self.trie_add(list_words)
 
 
-	# Takes a list of words and adds all of them to the trie
-	def add_to_trie(self, listy): 
+	# Add function
+	def trie_add(self, list_words): 
 	
-		for word in listy:
+		for word in list_words:
 
 			current_node = self.root
 
-			# We search for each letter of the current word on 
-			# the next level of the trie by looking for it in the
-			# current TrieNode's children. If it isn't there, we 
-			# add it and keep going. 
+			# If child not found then add the letter as child
 			for letter in list(word):
 				next_node = current_node.get_child(letter)
 				if next_node == None:
@@ -60,7 +47,5 @@ class Trie:
 					current_node.add(next_node)
 				current_node = next_node
 
-			# Once each word has completed, we add it to the 'complete'
-			# field of the current node to indicate that the current node,
-			# which iss the last letter in the word, completes that word.
+			# This is a flag in Trie to see if word is complete
 			current_node.complete = word
